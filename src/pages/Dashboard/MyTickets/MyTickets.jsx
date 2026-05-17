1379cda5ad7c392458f13156a870181ca665fe63
riday-kumar <hridoykumarsaha07@gmail.com>
2026-05-17 11:25:38 +0600

    ticket update done

--- src/pages/Dashboard/MyTickets/MyTickets.jsx
+++ src/pages/Dashboard/MyTickets/MyTickets.jsx
@@ -1,20 +1,93 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useAuth from "../../../hooks/useAuth";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 const MyTickets = () => {
   const { user } = useAuth();
   const email = user?.email;
+
   const axiosSecure = useAxiosSecure();
 
-  const { data: myAddedTickets = [] } = useQuery({
+  const [selectedTicket, setSelectedTicket] = useState(null);
+
+  const modalRef = useRef("modalRef");
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const { data: myAddedTickets = [], refetch } = useQuery({
     queryKey: ["myAddedTickets", email],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/tickets/${email}`);
+      const res = await axiosSecure.get(`/tickets?email=${email}`);
       return res.data;
     },
   });
 
-  console.log(myAddedTickets);
+  const showDataInUpdateForm = (ticket) => {
+    modalRef.current.showModal();
+    setSelectedTicket(ticket);
+
+    reset({
+      ticketTitle: ticket.ticketTitle,
+      ticketFrom: ticket.ticketFrom,
+      ticketTo: ticket.ticketTo,
+      transportType: ticket.transportType,
+      ticketPrice: ticket.ticketPrice,
+      ticketQuantity: ticket.ticketQuantity,
+      departureTime: ticket.departureTime,
+      perks: ticket.perks,
+    });
+  };
+
+  const handleUpdateForm = (data) => {
+    console.log(data);
+    const ticketTitle = data.ticketTitle;
+    const ticketFrom = data.ticketFrom;
+    const ticketTo = data.ticketTo;
+    const transportType = data.transportType;
+    const ticketPrice = data.ticketPrice;
+    const ticketQuantity = data.ticketQuantity;
+    const departureTime = data.departureTime;
+    const perks = data.perks;
+
+    const updatedTicket = {
+      ticketTitle,
+      ticketFrom,
+      ticketTo,
+      transportType,
+      ticketPrice,
+      ticketQuantity,
+      departureTime,
+      perks,
+    };
+
+    console.log(updatedTicket);
+
+    const selectedId = selectedTicket._id;
+
+    axiosSecure.patch(`/tickets/${selectedId}`, updatedTicket).then((res) => {
+      if (res.data.acknowledged) {
+        modalRef.current.close();
+        refetch();
+        toast.success("ticket update successfully", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      }
+    });
+  };
+
+  const handleHideModal = (e) => {
+    e.preventDefault();
+    modalRef.current.close();
+  };
   return (
     <div>
       <div className="max-w-7xl mx-auto px-4 py-10">
@@ -92,6 +165,7 @@ const MyTickets = () => {
                   {/* Buttons */}
                   <div className="card-actions justify-end mt-5">
                     <button
+                      onClick={() => showDataInUpdateForm(ticket)}
                       disabled={isRejected}
                       className="btn btn-primary btn-sm"
                     >
@@ -118,6 +192,163 @@ const MyTickets = () => {
           })}
         </div>
       </div>
+
+      {/* --------------------- modal ------------------- */}
+      <dialog
+        ref={modalRef}
+        id="my_modal_5"
+        className="modal modal-bottom sm:modal-middle"
+      >
+        <div className="modal-box">
+          <h3 className="font-bold text-lg text-center">Update Ticket</h3>
+          <div className="modal-action">
+            <form
+              onSubmit={handleSubmit(handleUpdateForm)}
+              className="dialogue space-y-5"
+            >
+              {/* Ticket Title */}
+              <div>
+                <label className="font-semibold">Ticket Title</label>
+                <input
+                  type="text"
+                  {...register("ticketTitle")}
+                  placeholder="Enter ticket title"
+                  className="input input-bordered w-full mt-1"
+                />
+              </div>
+
+              {/* From & To */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label className="font-semibold">From</label>
+                  <input
+                    type="text"
+                    {...register("ticketFrom")}
+                    placeholder="From location"
+                    className="input input-bordered w-full mt-1"
+                  />
+                </div>
+
+                <div>
+                  <label className="font-semibold">To</label>
+                  <input
+                    type="text"
+                    {...register("ticketTo")}
+                    placeholder="To location"
+                    className="input input-bordered w-full mt-1"
+                  />
+                </div>
+              </div>
+
+              {/* Transport Type */}
+              <div>
+                <label className="font-semibold">Transport Type</label>
+
+                <select
+                  value={selectedTicket?.transportType}
+                  {...register("transportType")}
+                  className="select select-bordered w-full mt-1"
+                >
+                  <option value="">Select Transport</option>
+                  <option value="Bus">Bus</option>
+                  <option value="Train">Train</option>
+                  <option value="Flight">Flight</option>
+                  <option value="Ship">Ship</option>
+                </select>
+              </div>
+
+              {/* Price & Quantity */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label className="font-semibold">Price (Per Unit)</label>
+
+                  <input
+                    type="number"
+                    {...register("ticketPrice")}
+                    placeholder="Enter price"
+                    className="input input-bordered w-full mt-1"
+                  />
+                </div>
+
+                <div>
+                  <label className="font-semibold">Ticket Quantity</label>
+
+                  <input
+                    type="number"
+                    {...register("ticketQuantity")}
+                    placeholder="Available tickets"
+                    className="input input-bordered w-full mt-1"
+                  />
+                </div>
+              </div>
+
+              {/* Departure Date & Time */}
+              <div>
+                <label className="font-semibold">Departure Date & Time</label>
+
+                <input
+                  type="datetime-local"
+                  {...register("departureTime")}
+                  className="input input-bordered w-full mt-1"
+                />
+              </div>
+
+              {/* Perks */}
+              <div>
+                <label className="font-semibold block mb-2">Perks</label>
+
+                <div className="flex flex-wrap gap-4">
+                  <label className="flex items-center gap-2">
+                    <input
+                      type="checkbox"
+                      value="AC"
+                      {...register("perks")}
+                      className="checkbox"
+                    />
+                    AC
+                  </label>
+
+                  <label className="flex items-center gap-2">
+                    <input
+                      type="checkbox"
+                      value="Breakfast"
+                      {...register("perks")}
+                      className="checkbox"
+                    />
+                    Breakfast
+                  </label>
+
+                  <label className="flex items-center gap-2">
+                    <input
+                      type="checkbox"
+                      value="Wifi"
+                      {...register("perks")}
+                      className="checkbox"
+                    />
+                    Wifi
+                  </label>
+
+                  <label className="flex items-center gap-2">
+                    <input
+                      type="checkbox"
+                      value="Charging Port"
+                      {...register("perks")}
+                      className="checkbox"
+                    />
+                    Charging Port
+                  </label>
+                </div>
+              </div>
+
+              {/* Submit Button */}
+              <button className="btn btn-primary w-full">Update Ticket</button>
+              <button onClick={handleHideModal} className="btn">
+                Close
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
